03ca32523449f9b390adef8f08c70d00308ddc56
Clémence <clemence.faligot@viacesi.fr>
2021-12-02 23:22:02 +0100

    Ajout vue xaml language

--- WpfAppEasySave/WpfAppEasySave/ViewModel/LanguageViewModel.cs
+++ WpfAppEasySave/WpfAppEasySave/ViewModel/LanguageViewModel.cs
@@ -8,54 +8,84 @@ namespace Language
 {
     class LanguageViewModel
     {
-        public void LanguageC()
+        public void Button_FR()
         {
-            /* Affichage des différentes langues et attente du choix de la part de l'utilisateur */
-            int iChoiceL = 0;
-
-            while (iChoiceL != 3)
+            /*LanguageView languageView = new LanguageView();
+            languageView.LanguageV();*/
+            try
             {
-                try
-                {
-                    LangView LanguageView = new LangView();
-                    LanguageView.LanguageV();
+                string sLang = "FR";
+                LanguageModel.sCurrentLanguage = sLang;
+            }
+            catch
+            {
 
-                    iChoiceL = int.Parse(Console.ReadLine());
-
-                    /* Change la langue en français */
-                    if (iChoiceL == 1)
-                    {
-                        string sLang = "FR";
-                        LanguageModel.sCurrentLanguage = sLang;
-                        Console.Clear();
-
-                    }
-
-                    /* Change la langue en anglais */
-                    else if (iChoiceL == 2)
-                    {
-                        string sLang = "EN";
-                        LanguageModel.sCurrentLanguage = sLang;
-                        Console.Clear();
-                    }
-
-                    /* Affichage d'une erreur en cas de saisie invalide de l'utilisateur */
-                    else if (iChoiceL > 3)
-                    {
-                        ErrorViewModel errorController = new ErrorViewModel();
-                        errorController.ErrorC();
-                        Console.Clear();
-                    }
-                }
-
-                /* Affichage d'une erreur en cas de disfonctionnement */
-                catch
-                {
-                    ErrorViewModel errorController = new ErrorViewModel();
-                    errorController.ErrorC();
-                    Console.Clear();
-                }
             }
         }
+
+        public void Button_EN()
+        {
+            /*LanguageView languageView = new LanguageView();
+            languageView.LanguageV();*/
+            try
+            {
+                string sLang = "EN";
+                LanguageModel.sCurrentLanguage = sLang;
+            }
+            catch
+            {
+
+            }
+        }
+
+        /* public void LanguageC()
+         {
+             /* Affichage des différentes langues et attente du choix de la part de l'utilisateur 
+             int iChoiceL = 0;
+
+             while (iChoiceL != 3)
+             {
+                 try
+                 {
+                     LangView LanguageView = new LangView();
+                     LanguageView.LanguageV();
+
+                     iChoiceL = int.Parse(Console.ReadLine());
+
+                     /* Change la langue en français 
+                     if (iChoiceL == 1)
+                     {
+                         string sLang = "FR";
+                         LanguageModel.sCurrentLanguage = sLang;
+                         Console.Clear();
+
+                     }
+
+                     /* Change la langue en anglais 
+                     else if (iChoiceL == 2)
+                     {
+                         string sLang = "EN";
+                         LanguageModel.sCurrentLanguage = sLang;
+                         Console.Clear();
+                     }
+
+                     /* Affichage d'une erreur en cas de saisie invalide de l'utilisateur 
+                     else if (iChoiceL > 3)
+                     {
+                         ErrorViewModel errorController = new ErrorViewModel();
+                         errorController.ErrorC();
+                         Console.Clear();
+                     }
+                 }
+
+                 /* Affichage d'une erreur en cas de disfonctionnement 
+                 catch
+                 {
+                     ErrorViewModel errorController = new ErrorViewModel();
+                     errorController.ErrorC();
+                     Console.Clear();
+                 }
+             }
+         }*/
     }
 }
