9ecc2536e1d676432b011f3a57cd3019ddfbff6d
Justine4s <justine.quatresols@viacesi.fr>
2021-12-03 09:15:57 +0100

    Création des pages + bouton retour + Mise en place rapide du menu

--- WpfAppEasySave/WpfAppEasySave/MainWindow.xaml.cs
+++ WpfAppEasySave/WpfAppEasySave/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfAppEasySave.View;
 
 namespace WpfAppEasySave
 {
@@ -23,6 +24,25 @@ namespace WpfAppEasySave
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void Ouvrir_Langue(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new PageLangue();
+        }
+
+        private void Ouvrir_Sauvegarde(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new PageSauvegarde();
+        }
+
+        private void Ouvrir_Log(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new PageLog();
+        }
+
+        private void Ouvrir_Etat(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new PageEtat();
         }
     }
 }
---
+++ WpfAppEasySave/WpfAppEasySave/View/PageEtat.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppEasySave.View
+{
+    /// <summary>
+    /// Logique d'interaction pour PageEtat.xaml
+    /// </summary>
+    public partial class PageEtat : Page
+    {
+        public PageEtat()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Retour(object sender, RoutedEventArgs e)
+        {
+            Content = null;
+        }
+    }
+}
